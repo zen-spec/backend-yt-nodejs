d00bbf503b8a88c926e568881da0c97e49a0359e
zen <159615496+zen-spec@users.noreply.github.com>
2026-09-23 20:18:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { TiktokDL } = require('@tobyg74/tiktok-api-dl');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 4914;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
